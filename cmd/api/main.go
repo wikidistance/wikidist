8f88478ea4ef0938ee6cdbfcada7aba549da32ae
Mayeul Blanzat <may-26@hotmail.fr>
2019-11-28 14:22:10 +0100

    Fix missing trailing new line

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,13 +1,13 @@
 package main
 
 import (
-	"net/http"
 	"log"
+	"net/http"
 
 	"github.com/wikidistance/wikidist/pkg/api"
 )
 
-func main(){
-    http.HandleFunc("/", api.DefaultHandler)
-    log.Fatal(http.ListenAndServe(":8080", nil))
+func main() {
+	http.HandleFunc("/", api.DefaultHandler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
