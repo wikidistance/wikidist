063c0c5eb2cc8dbf0876ee0b7bd2ef813ca21217
Mayeul Blanzat <may-26@hotmail.fr>
2020-01-13 15:18:33 +0100

    Add a new API route to search nodes by uid

--- pkg/api/api.go
+++ pkg/api/api.go
@@ -23,12 +23,13 @@ func DefaultHandler(w http.ResponseWriter, r *http.Request) {
 
 func (dg *DGraph) ShortestPathHandler(w http.ResponseWriter, r *http.Request) {
 	dg2 := (*db.DGraph)(dg)
+
 	from, ok := r.URL.Query()["from"]
-	fmt.Fprintf(w, "from[0] = %s, from = %s", from[0], from)
 	if !ok || len(from[0]) < 1 {
 		fmt.Fprint(w, "Need a from argument")
 		return
 	}
+
 	to, ok := r.URL.Query()["to"]
 	if !ok || len(to[0]) < 1 {
 		fmt.Fprint(w, "Need a to argument")
@@ -65,6 +66,32 @@ func (dg *DGraph) PageSearchHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	res = dg.PageSearch(search.Search, search.Depth)
+
+	json.NewEncoder(w).Encode(res)
+}
+
+func (dg *DGraph) UidSearchHandler(w http.ResponseWriter, r *http.Request) {
+	var search Search
+	var res []db.Article
+
+	w.Header().Set("Content-type", "application/json;charset=UTF-8")
+	w.WriteHeader(http.StatusOK)
+
+	reqBody, err := ioutil.ReadAll(r.Body)
+
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintf(w, "Please enter a valid uid")
+	}
+
+	err = json.Unmarshal(reqBody, &search)
+
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintf(w, "Unable to parse the body")
+	}
+
+	res = dg.UidSearch(search.Search, search.Depth)
 
 	json.NewEncoder(w).Encode(res)
 }
