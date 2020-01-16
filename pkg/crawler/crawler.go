224ae99792a596541461504241b4d517c0e088d9
Ronan Pelliard <ronan.pelliard@runbox.com>
2020-01-16 15:38:26 +0100

    remove rate limit

--- pkg/crawler/crawler.go
+++ pkg/crawler/crawler.go
@@ -17,7 +17,7 @@ const resultQueueSizeFactor = 2
 const refillFactor = 3 * queueSizeFactor / 2
 
 // requests per minute
-const rateLimit = 3000
+const rateLimit = 100000
 
 type Crawler struct {
 	nWorkers   int
